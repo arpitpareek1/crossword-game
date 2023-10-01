e3281b8cbf422603bc326cd45784453fd004796a
Arpit pareek <arpitpareek058@gmail.com>
2023-10-02 01:28:26 +0530

    setup and create crossword sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Crossword from "@funifier/react-crossword";
+import React from "react";
 
-function App() {
+export default function App() {
+  const data = {
+    across: {
+      1: {
+        clue: "one plus one",
+        answer: "TWO",
+        row: 0,
+        col: 0,
+      },
+      4: {
+        clue: "a female parent",
+        answer: "MOTHER",
+        row: 2,
+        col: 1,
+      },
+      7: {
+        clue: "opposite of 'close'",
+        answer: "OPEN",
+        row: 4,
+        col: 2,
+      },
+    },
+    down: {
+      2: {
+        clue: "three minus two",
+        answer: "ONE",
+        row: 0,
+        col: 2,
+      },
+      3: {
+        clue: "a flying mammal",
+        answer: "BAT",
+        row: 1,
+        col: 3,
+      },
+      5: {
+        clue: "a large, gray animal",
+        answer: "ELEPHANT",
+        row: 2,
+        col: 5,
+      },
+      6: {
+        clue: "where you might take a shower",
+        answer: "BATHROOM",
+        row: 3,
+        col: 4,
+      },
+    },
+  };
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello CodeSandbox</h1>
+      <div style={{ width: "70%", height: "70%" }}>
+        <Crossword
+          data={data}
+          onAnswerComplete={(d, n, c, s) => {
+            console.log("dncs", d, n, c, s);
+          }}
+        />
+      </div>
     </div>
   );
 }
-
-export default App;
